903465e617845cfc05b2f6f54fe9245dd1c719b5
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2018-05-03 21:38:04 +0700

    update user

--- EFCore/Demo/Models/User.cs
+++ EFCore/Demo/Models/User.cs
@@ -6,11 +6,21 @@ namespace Demo.Models
 {
     public class User
     {
+        public User()
+        {
+        }
+
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public ICollection<Post> Posts { get; set; }
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
 
-        public ICollection<Reply>   Replies { get; set; }
+        public ICollection<Reply> Replies { get; set; } = new List<Reply>();
     }
 }
