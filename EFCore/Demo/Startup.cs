303a5fdf4e4d75394cf6fe3ba0ec4bf4a8833c1a
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2018-05-03 21:44:58 +0700

    add user

--- EFCore/Demo/Startup.cs
+++ EFCore/Demo/Startup.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
 namespace Demo
 {
-   public class Startup
+    using Demo.Data;
+    using Demo.Models;
+
+    public class Startup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var context = new DemoDbContext();
+
+            var users = new[]
+            {
+                new User("thanhthinh","123456"),
+
+                new User("vanteo","7654321"),
+            };
+
+            context.Users.AddRange(users);
+
+            context.SaveChanges();
+
         }
     }
 }
